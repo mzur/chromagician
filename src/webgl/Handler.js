d46bc07001f3a808acd987ccdb5af19021c9ca1d
Martin Zurowietz <martin@zurowietz.de>
2025-10-07 21:38:02 +0200

    Fix texture y axis

--- src/webgl/Handler.js
+++ src/webgl/Handler.js
@@ -73,6 +73,8 @@ export default class Handler {
         array = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]);
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.bufferData(gl.ARRAY_BUFFER, array, gl.STATIC_DRAW);
+
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     }
 
     compileShader_(gl, shader, source) {
