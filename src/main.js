b0f57e76296adc00c7f714e9db3089863fca03c6
Martin Zurowietz <martin@zurowietz.de>
2025-11-28 22:00:23 +0100

    Remove while balance program

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import DenoiseProgram from './webgl/DenoiseProgram.js';
 import DetectColorProgram from './webgl/DetectColorProgram.js';
 import WebglHandler from './webgl/Handler.js';
-import WhiteBalanceProgram from './webgl/WhiteBalanceProgram.js';
 import RenderPatternProgram from './webgl/RenderPatternProgram.js';
-// import { FastAverageColor } from 'fast-average-color';
 
 const video = document.createElement('video');
 const canvas = document.getElementById("canvas");
 const FPS_INTERVAL = 1000 / 10;
 const flipY = false;
 const colorSwitch = 1;
-
-// TODO
-// const fac = new FastAverageColor();
-// let whiteBalance
 
 let handler;
 let patternProgram;
@@ -55,10 +49,6 @@ navigator.mediaDevices.getUserMedia({video: cameraConstraints})
    window.addEventListener('beforeunload', handler.destruct.bind(handler));
    let denoise = new DenoiseProgram(video);
    handler.addProgram(denoise);
-   // TODO: Should we use white balance or not? Better algorithm?
-   // whiteBalance = new WhiteBalanceProgram(video);
-   // handler.addProgram(whiteBalance);
-   // whiteBalance.link(denoise);
    let detectProgram = new DetectColorProgram(video);
    handler.addProgram(detectProgram);
    detectProgram.link(denoise);
@@ -81,9 +71,6 @@ navigator.mediaDevices.getUserMedia({video: cameraConstraints})
 
       if (deltaTime > FPS_INTERVAL) {
          lastTime = currentTime - (deltaTime % FPS_INTERVAL);
-         // TODO
-         // whiteBalance.setAvgColor(fac.getColor(video).value);
-
          handler.render();
       }
 
